500821ef07ea55e27acb398eaa2b177ef58110b4
Sabohatfrontend <ssobirova333@gmail.com>
2024-05-09 14:52:52 +0500

    fix: fix errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:@typescript-eslint/recommended-type-checked",
