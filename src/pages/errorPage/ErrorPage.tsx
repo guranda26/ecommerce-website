3e7fd9a52861d22e05e894564a9ac199bc0d8879
Sabohatfrontend <ssobirova333@gmail.com>
2024-05-14 06:06:16 +0500

    fix: format errorPage file

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,26 +1,29 @@
 import { ErrorResponse, NavLink, useRouteError } from 'react-router-dom';
 import errorImage from '../../assets/images/404-error.png';
-import planetImage from '../../assets/images/planet.png'
+import planetImage from '../../assets/images/planet.png';
 
 function ErrorPage() {
   const error = useRouteError() as ErrorResponse;
   if (error.status === 404) {
     return (
-      <div className='error-container'>
+      <div className="error-container">
         <img src={errorImage} alt="Error page image" width={400} height={400} />
-        <h2 className='header-content'>PageNotFound</h2>
-        <NavLink className='home-page-btn' to={'./main'}>Home Page</NavLink>
+        <h2 className="header-content">PageNotFound</h2>
+        <NavLink className="home-page-btn" to={'./main'}>
+          Home Page
+        </NavLink>
       </div>
-    )
+    );
   }
   return (
-    <div className='error-container'>
+    <div className="error-container">
       <img src={planetImage} alt="Error page image" width={400} height={400} />
-      <h2 className='header-content'>Something went wrong:</h2>
-      <NavLink className='home-page-btn' to={'./main'}>Home Page</NavLink>
+      <h2 className="header-content">Something went wrong:</h2>
+      <NavLink className="home-page-btn" to={'./main'}>
+        Home Page
+      </NavLink>
     </div>
-  )
-
+  );
 }
 
 export default ErrorPage;
