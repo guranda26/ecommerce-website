8a9fa3850efed792ba6f16b38d997eaf4c75a60e
Sabohatfrontend <ssobirova333@gmail.com>
2024-05-16 07:46:59 +0500

    feat: add services section

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Features from '../../components/features/Features';
+import Support from '../../components/services/Services';
 
 function MainPage(): React.JSX.Element {
   return (
     <>
       <Features />
+      <Support />
     </>
   );
 }
