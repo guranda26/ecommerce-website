f6d0467ea2fe16034fe8f29b7f7b5530dc59a83e
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-05-17 18:10:12 +0400

    Update path to RegistrationForm

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
       children: [
         {
           index: true,
-          element: <Register />,
+          element: <RegistrationForm />,
         },
       ],
     },
