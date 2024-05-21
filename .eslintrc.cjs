931ed83750ced7689e0a2aa7536873df9de49c58
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-05-21 17:34:57 +0400

    Revert "Merge pull request #10 from Sabohatfrontend/feature-develop"
    This reverts commit ba25ccb83883eb2900172e2800f2d2736e9be308, reversing
    changes made to 47a8d7d8f56a695fad89164d5671dbe39c3c15ea.

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+  },
+};
