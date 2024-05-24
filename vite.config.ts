19354673ffee0e72aa30c214863104217ea44c02
Sabohatfrontend <ssobirova333@gmail.com>
2024-05-24 15:16:31 +0500

    fix: add rollupOption

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,17 @@ export default defineConfig(({ mode }) => {
       },
     },
     plugins: [react(), nodePolyfills({ include: ['process'] })],
-    build: { chunkSizeWarningLimit: 1600, },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        }
+      }
+    },
     base: './',
     resolve: {
       alias: {
