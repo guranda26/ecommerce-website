f211022f2aeeab3688653ab1d525b35e4f073275
Sabohat <ssobirova333@gmail.com>
2024-06-04 16:43:06 +0500

    feat: add style to catalog page

--- src/pages/catalog/ProductsList.tsx
+++ src/pages/catalog/ProductsList.tsx
@@ -2,6 +2,7 @@ import { Product } from '@commercetools/platform-sdk';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './catalog.css';
+import { getPrice } from '../../components/priceFunction/getPrice';
 
 function PoductsList(props: { products: Product[] }): React.JSX.Element {
   const navigate = useNavigate();
@@ -11,21 +12,53 @@ function PoductsList(props: { products: Product[] }): React.JSX.Element {
     navigate(`/catalog/${id}`);
   };
 
+  const cutDescription = (text: string) => text.split('.')[0];
+
   return (
     <ul className="products-list">
       {products &&
         products.map((product) => (
-          <li key={product.id} onClick={() => productClickHandle(product.id)}>
-            <img
-              src={
-                product.masterData.staged.masterVariant.images![0].url ||
-                'https://placehold.co/300x300.png?text=Without+Image'
-              }
-              alt={`${product.masterData.staged.name['en-US']} image`}
-              width={300}
-              height={300}
-            />
-            <h3>{product.masterData.staged.name['en-US']}</h3>
+          <li
+            className="products-item"
+            key={product.id}
+            onClick={() => productClickHandle(product.id)}
+          >
+            <div className="products-item-top">
+              <img
+                className="products-img"
+                src={
+                  product.masterData.current.masterVariant.images![0].url ||
+                  'https://placehold.co/300x300.png?text=Without+Image'
+                }
+                alt={`${product.masterData.current.name['en-US']} image`}
+                width={200}
+                height={200}
+              />
+            </div>
+            <div className="products-item-bottom">
+              <h3 className="products-header">
+                {product.masterData.current.name['en-US']}
+              </h3>
+              <div className="price-content">
+                <span
+                  className={
+                    getPrice(product.masterData.current).discountPrice
+                      ? 'price discount-price'
+                      : 'price'
+                  }
+                >
+                  {getPrice(product.masterData.current).price}
+                </span>
+                <span className="price discount">
+                  {getPrice(product.masterData.current).discountPrice}
+                </span>
+              </div>
+              <p className="products-description">
+                {cutDescription(
+                  product.masterData.current.description!['en-US']
+                )}
+              </p>
+            </div>
           </li>
         ))}
     </ul>
