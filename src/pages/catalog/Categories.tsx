87fea65fa6468182ddf1b63e96460e264e9e7571
Sabohat <ssobirova333@gmail.com>
2024-06-06 12:53:21 +0500

    feat: add catalog name

---
+++ src/pages/catalog/Categories.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Category } from '@commercetools/platform-sdk';
+import { clientMaker } from '../../../sdk/createClient';
+import CategoriesList from './CategoriesList';
+function Categories(): React.JSX.Element {
+  const [response, setResponse] = useState<Category[]>([]);
+
+  const fetchData = useCallback(async () => {
+    const apiRoot = clientMaker();
+    const getProducts = async () => {
+      try {
+        const response = await apiRoot.categories().get().execute();
+        return response;
+      } catch (error) {
+        let errorMessage = 'Unknown error';
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        throw new Error(`Failed to get products: ${errorMessage}`);
+      }
+    };
+
+    try {
+      const res = await getProducts();
+      if (res.statusCode === 200) {
+        setResponse(() => [...res.body.results]);
+      }
+    } catch (error: unknown) {
+      let message = 'Unknown Error';
+      if (error instanceof Error) message = error.message;
+      console.log(message);
+    }
+  }, []);
+
+  useEffect(() => {
+    void fetchData();
+  }, [fetchData]);
+
+  console.log(response);
+  return (
+    <>
+      <CategoriesList categories={response} />
+    </>
+  );
+}
+
+export default Categories;
