916897f38e8ee851cd95bb22bd2264fa3f750044
Sabohat <ssobirova333@gmail.com>
2024-06-06 21:46:29 +0500

    feat: add total products

--- src/pages/catalog/ProductsList.tsx
+++ src/pages/catalog/ProductsList.tsx
@@ -17,49 +17,52 @@ function PoductsList(props: {
   const cutDescription = (text: string) => text.split('.')[0];
 
   return (
-    <ul className="products-list">
-      {products &&
-        products.map((product) => (
-          <li
-            className="products-item"
-            key={product.id}
-            onClick={() => productClickHandle(product.id)}
-          >
-            <div className="products-item-top">
-              <img
-                className="products-img"
-                src={
-                  product.masterVariant.images![0].url ||
-                  'https://placehold.co/300x300.png?text=Without+Image'
-                }
-                alt={`${product.name['en-US']} image`}
-                width={200}
-                height={200}
-              />
-            </div>
-            <div className="products-item-bottom">
-              <h3 className="products-header">{product.name['en-US']}</h3>
-              <div className="price-content">
-                <span
-                  className={
-                    getPrice(product).discountPrice
-                      ? 'price discount-price'
-                      : 'price'
+    <>
+      <p className='total'>Total Products: <span>{products.length}</span></p>
+      <ul className="products-list">
+        {products &&
+          products.map((product) => (
+            <li
+              className="products-item"
+              key={product.id}
+              onClick={() => productClickHandle(product.id)}
+            >
+              <div className="products-item-top">
+                <img
+                  className="products-img"
+                  src={
+                    product.masterVariant.images![0].url ||
+                    'https://placehold.co/300x300.png?text=Without+Image'
                   }
-                >
-                  {getPrice(product).price}
-                </span>
-                <span className="price discount">
-                  {getPrice(product).discountPrice}
-                </span>
+                  alt={`${product.name['en-US']} image`}
+                  width={200}
+                  height={200}
+                />
               </div>
-              <p className="products-description">
-                {cutDescription(product.description!['en-US'])}
-              </p>
-            </div>
-          </li>
-        ))}
-    </ul>
+              <div className="products-item-bottom">
+                <h3 className="products-header">{product.name['en-US']}</h3>
+                <div className="price-content">
+                  <span
+                    className={
+                      getPrice(product).discountPrice
+                        ? 'price discount-price'
+                        : 'price'
+                    }
+                  >
+                    {getPrice(product).price}
+                  </span>
+                  <span className="price discount">
+                    {getPrice(product).discountPrice}
+                  </span>
+                </div>
+                <p className="products-description">
+                  {cutDescription(product.description!['en-US'])}
+                </p>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 
