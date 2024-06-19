2497d8e4d022947d1d771d6a4d89e7f256b54f0a
Sabohat <ssobirova333@gmail.com>
2024-06-19 06:04:50 +0500

    feat: add loading to addinf and deleting product from basket

--- src/components/basket/BasketButton.tsx
+++ src/components/basket/BasketButton.tsx
@@ -1,5 +1,5 @@
 import { ProductProjection } from '@commercetools/platform-sdk';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './basket.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -14,6 +14,7 @@ import {
   isExistProductMyCart,
 } from '../../../sdk/basketApi';
 import { UserContext } from '../../../src/context/userContext';
+import { CircularProgress } from '@mui/material';
 
 function BasketButton(props: {
   product: ProductProjection;
@@ -25,88 +26,114 @@ function BasketButton(props: {
     cart ? isExistProductMyCart(product.id, cart) : true
   );
 
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
   const handleBasket = async (product: ProductProjection) => {
+    setLoading(true);
     const response = await addProductToCart(product, number, cart!, apiRoot!);
     if (response?.statusCode === 200) {
       setDisabled(true);
       setCart(response.body);
     }
+    else {
+      setErrorMessage(true);
+    }
+    setLoading(false);
   };
 
   const deleteProduct = (product: ProductProjection) => {
     if (cart && apiRoot) {
       void (async () => {
+        setLoading(true);
         const response = await deleteProductInCart(product, cart, apiRoot);
         if (response?.statusCode === 200) {
           setDisabled(false);
         }
+        else {
+          setErrorMessage(true);
+        }
+        setLoading(false);
       })();
     }
+
   };
+
+  useEffect(() => {
+    if (errorMessage === true) {
+      setTimeout(() => {
+        setErrorMessage(false);
+      }, 500)
+    }
+  }, [errorMessage]);
 
   return (
     <>
-      <div className="basket">
-        {product.masterVariant.availability?.availableQuantity &&
-        !isDisabled ? (
-          <div className="basket-wrap">
-            <div className="num-wrap">
-              <button
-                className="num-btn"
-                title={`Remove one pcs ${product.name['en-US']}`}
-                onClick={() =>
-                  setNumber((prev) => {
-                    if (prev > 1) return prev - 1;
-                    return prev;
-                  })
-                }
-              >
-                {<FontAwesomeIcon className="num-image" icon={faMinus} />}
-              </button>
-              <p className="num">{number}</p>
-              <button
-                className="num-btn"
-                title={`Add one pcs ${product.name['en-US']}`}
-                onClick={() =>
-                  setNumber((prev) => {
-                    if (
-                      product.masterVariant?.availability?.availableQuantity &&
-                      prev <
-                        product.masterVariant?.availability?.availableQuantity
-                    )
-                      return prev + 1;
-                    return prev;
-                  })
-                }
-              >
-                {<FontAwesomeIcon className="num-image" icon={faPlus} />}
-              </button>
-            </div>
-            <button
-              className="basket-btn"
-              title={`Add ${number} pcs ${product.name['en-US']} to basket`}
-              onClick={() => void handleBasket(product)}
-            >
-              <FontAwesomeIcon className="basket-image" icon={faCartPlus} />
-            </button>
-          </div>
-        ) : !product.masterVariant.availability?.availableQuantity ? (
-          <button
-            className="basket-btn disabled"
-            title={`Cannot add ${product.name['en-US']} to basket`}
-          >
-            <FontAwesomeIcon className="basket-image" icon={faCartShopping} />
-          </button>
-        ) : (
-          <button
-            className="basket-btn disabled"
-            title={`Remove ${product.name['en-US']} from basket`}
-            onClick={() => deleteProduct(product)}
-          >
-            <FontAwesomeIcon className="basket-image" icon={faCartShopping} />
-          </button>
-        )}
-      </div>
+      {
+        loading ? <CircularProgress /> :
+          (
+            errorMessage ? <p className='basket-error'>There are some error</p> :
+              <div className="basket">
+                {product.masterVariant.availability?.availableQuantity &&
+                  !isDisabled ? (
+                  <div className="basket-wrap">
+                    <div className="num-wrap">
+                      <button
+                        className="num-btn"
+                        title={`Remove one pcs ${product.name['en-US']}`}
+                        onClick={() =>
+                          setNumber((prev) => {
+                            if (prev > 1) return prev - 1;
+                            return prev;
+                          })
+                        }
+                      >
+                        {<FontAwesomeIcon className="num-image" icon={faMinus} />}
+                      </button>
+                      <p className="num">{number}</p>
+                      <button
+                        className="num-btn"
+                        title={`Add one pcs ${product.name['en-US']}`}
+                        onClick={() =>
+                          setNumber((prev) => {
+                            if (
+                              product.masterVariant?.availability?.availableQuantity &&
+                              prev <
+                              product.masterVariant?.availability?.availableQuantity
+                            )
+                              return prev + 1;
+                            return prev;
+                          })
+                        }
+                      >
+                        {<FontAwesomeIcon className="num-image" icon={faPlus} />}
+                      </button>
+                    </div>
+                    <button
+                      className="basket-btn"
+                      title={`Add ${number} pcs ${product.name['en-US']} to basket`}
+                      onClick={() => void handleBasket(product)}
+                    >
+                      <FontAwesomeIcon className="basket-image" icon={faCartPlus} />
+                    </button>
+                  </div>
+                ) : !product.masterVariant.availability?.availableQuantity ? (
+                  <button
+                    className="basket-btn disabled"
+                    title={`Cannot add ${product.name['en-US']} to basket`}
+                  >
+                    <FontAwesomeIcon className="basket-image" icon={faCartShopping} />
+                  </button>
+                ) : (
+                  <button
+                    className="basket-btn disabled"
+                    title={`Remove ${product.name['en-US']} from basket`}
+                    onClick={() => deleteProduct(product)}
+                  >
+                    <FontAwesomeIcon className="basket-image" icon={faCartShopping} />
+                  </button>
+                )}
+              </div>)}
     </>
   );
 }
