08914fe7d78c45dd2271025191131e6bdff7afb7
Sabohat <ssobirova333@gmail.com>
2024-06-19 14:38:40 +0500

    feat: add style progress bar

--- src/components/basket/BasketButton.tsx
+++ src/components/basket/BasketButton.tsx
@@ -68,13 +68,13 @@ function BasketButton(props: {
   return (
     <>
       {loading ? (
-        <CircularProgress />
+        <CircularProgress className='progress' disableShrink />
       ) : errorMessage ? (
         <p className="basket-error">There are some error</p>
       ) : (
         <div className="basket">
           {product.masterVariant.availability?.availableQuantity &&
-          !isDisabled ? (
+            !isDisabled ? (
             <div className="basket-wrap">
               <div className="num-wrap">
                 <button
@@ -99,7 +99,7 @@ function BasketButton(props: {
                         product.masterVariant?.availability
                           ?.availableQuantity &&
                         prev <
-                          product.masterVariant?.availability?.availableQuantity
+                        product.masterVariant?.availability?.availableQuantity
                       )
                         return prev + 1;
                       return prev;
